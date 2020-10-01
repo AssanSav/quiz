5f8b3b9635ff986b809a4ae3a9dd61747eba9aaa
AssanSav <assane.savadogo81@gmail.com>
2020-09-30 21:11:40 -0400

    Shuffle array and and quizzes functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
-}
+const App = () => {
 
+  const shuffleArray = (array) => {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  };
+
+  let shuffleQuizzes = (array) => {
+    let result = [];
+    array.questions.forEach((quiz) => {
+      result.push({
+        text: quiz.text,
+        correctAnswer: quiz.correctAnswer,
+        answers: shuffleArray(quiz.incorrectAnswers.concat(quiz.correctAnswer)),
+      });
+    });
+    return result;
+  };
+
+
+  return (  );
+}
+ 
 export default App;
+
+
